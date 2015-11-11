9975a2434e31bffa92417ecf6a937cc620d7cbb5
Benedict Hobart <hobart.benedict@gmail.com>
2015-11-11 12:55:47 +1100

    feat: scaffolded routes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 let express = require('express'),
     path = require('path'),
-    mongoose = require('mongoose'),
-    routes = require('./routes');
+    mongoose = require('mongoose');
 
 let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes);
+app.get('/', (req, res) => res.render('index'));
+app.delete('/post/:id', function(req, res) {
+    res.send(req.params.id);
+});
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on ${port}`));
--- routes/index.js
+++
@@ -1,4 +0,0 @@
-let express = require('express'),
-    router = express.Router();
-
-module.exports = router;
