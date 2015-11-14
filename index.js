81fd247ca471ad664364d5f01b69185a6d86506f
Benedict Hobart <hobart.benedict@gmail.com>
2015-11-14 18:48:19 +1100

    feat: fix lint errors, scaffold frontend logic, update post logic

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 'use strict';
 const express = require('express'),
     path = require('path'),
+    debug = require('debug')('expressapp'),
     mongoose = require('mongoose'),
     posts = require('./posts');
 
+//Configure Mongoose
 const mongoURL = process.env.MONGO_URL || 'mongodb://localhost/hacker-news-feed';
 mongoose.connect(mongoURL);
 process.on('SIGINT', () => mongoose.connection.close(() => {
-    console.log('Mongoose disconnected');
+    debug('mongoose disconnected');
     process.exit(0);
 }));
 
 
-//Refresh posts every 60 minutes
+//Set refresh time
 posts.refresh();
 setInterval(posts.refresh, 60 * 60 * 1000);
 
+//Configure App
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    posts.find().then((posts) => {
-        res.render('index', {posts: posts});
-    });
+    posts.getLatestPosts()
+        .then((posts) => {
+            res.render('index', {posts: posts});
+        });
 });
+
 app.delete('/post/:id', function(req, res) {
-    posts.remove(req.params.id);
-    res.send(req.params.id);
+    posts.remove(req.params.id)
+        .then(() => res.send(req.params.id));
+});
+
+app.get('/refresh', function(req, res) {
+    posts.refresh();
+    res.send('REFRESH');
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on ${port}`));
+app.listen(port, () => debug(`listening on port ${port}`));
 
--- posts.js
+++ posts.js
@@ -1,47 +1,73 @@
 'use strict';
 const mongoose = require('mongoose'),
-    fetch = require('isomorphic-fetch');
+    fetch = require('isomorphic-fetch'),
+    debug = require('debug')('expressapp');
 
 const Post = mongoose.model('Post', {
     created_at: mongoose.Schema.Types.Date,
-    title: String,
     author: String,
+    title: String,
     story_title: String,
-    comment_text: String,
+    url: String,
     story_url: String,
     objectID: {
         type: String,
-        index: {
-            unique: true,
-            dropDups: true
-        }
+        unique: true
+    },
+    deleted: {
+        type: Boolean,
+        default: false
     }
 });
 
+
+/**
+ * saves the given post to the database
+ * @param  {object} post
+ */
+function savePost(post) {
+    if(!post.story_title && !post.title) {
+        debug(`did not add post ${post.objectID}`);
+        return;
+    }
+    new Post(post).save();
+    return;
+}
+
+/**
+ * gets the latest posts from the API and saves them to the database
+ */
 function refresh() {
-    fetch('http://hn.algolia.com/api/v1/search_by_date?query=nodejs')
+    debug('refreshing posts');
+    return fetch('http://hn.algolia.com/api/v1/search_by_date?query=nodejs')
         .then(res => res.json())
         .then(json => json.hits)
-        .then(posts => posts.forEach(post => new Post(post).save()));
+        .then(posts => posts.forEach(savePost));
 }
 
-function find() {
-    return Post.find({}).sort({
-        created_at: -1
-    }).limit(20);
+/**
+ * gets the latest posts from the database
+ * @return {Promise} - resolves with a list of up to 20 posts, rejects with Mongoose error
+ */
+function getLatestPosts() {
+    return Post.find({deleted: false})
+        .sort({created_at: -1})
+        .limit(20);
 }
 
+/**
+ * removes post with give objectID as deleted in db
+ * @param  {String} objectID
+ * @return {Promise}
+ */
 function remove(objectID) {
-    console.log(objectID);
-    Post.remove({
-        objectID
-    }, function () {
-        console.log(arguments);
-    });
+    return Post.findOneAndUpdate({objectID}, {
+        deleted: true
+    }).then(() => debug(`marked deleted post: ${objectID}`));
 }
 
 module.exports = {
     refresh,
-    find,
+    getLatestPosts,
     remove
 };
